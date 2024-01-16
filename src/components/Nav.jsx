ab46d687feb8277ce865c0398015b86fbf454633
Allan Njoroge <allandavis254@gmail.com>
2024-01-16 15:54:28 +0300

    Add Projects link

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,6 +9,7 @@ const Nav = () => {
     {name:"Home", link:"home"},
     {name:"About Me", link:"about"},
     {name:"Skills", link:"skills"},
+    {name:"Projects", link:"projects"},
     {name:"Contact", link:"contact"},
   ]
 
